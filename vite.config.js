d539d255312c843591f12bb82e609b578007396f
Riceroll <jianzhe.gu@gmail.com>
2025-11-14 21:55:06 -0600

    update the base to be Botai to enable github auto deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // ADD THIS LINE to set the base path for GitHub Pages project sites
+  base: '/Botai/', 
   plugins: [react()],
   server: {
     port: 3000,
